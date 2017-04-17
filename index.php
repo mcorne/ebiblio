4088574b8afc1cf5a458610b695197e7ae4f7636
Michel Corne <mcorne@yahoo.com>
2017-04-17 16:10:00 +0200

    fixed index

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <?php require 'common/header.php'; ?>
 
-<h1>eBiblio pour partager des livres</h1>
+<div class="w3-container">
 
-<p>
-    <a href="/ebiblio/restricted/booklist.php"><i class="fa fa-list" aria-hidden="true"></i></a>
-    Liste des livres
-</p>
+    <h1>eBiblio pour partager des livres</h1>
 
-<p>
-    <a href="/ebiblio/common/reset_password.php"><i class="fa fa-paper-plane" aria-hidden="true"></i></a>
-    Me renvoyer un mot de passe
-</p>
+</div>
+
+<div class="ul-panel">
+
+    <ul class="w3-ul">
+
+        <li class=" w3-border-0">
+            <a href="/ebiblio/restricted/booklist.php"><i class="fa fa-list" aria-hidden="true"></i></a>
+            Liste des livres
+        </li>
+
+        <li class=" w3-border-0">
+            <a href="/ebiblio/restricted/add_book.php"><i class="fa fa-list" aria-hidden="true"></i></a>
+            Me renvoyer un mot de passe
+        </li>
+
+    </ul>
+
+</div>
 
 <?php require 'common/footer.php'; ?>
