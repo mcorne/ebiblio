50b0ec527e5ae9548fcf7d31b5ff78153b37591a
Michel Corne <miccorne@gmail.com>
2017-05-17 14:53:49 +0200

    changed sub menu dropdown to accordion

--- application/common/header.php
+++ application/common/header.php
@@ -35,56 +35,48 @@
                 Liste des livres
             </a>
 
-            <div class="w3-dropdown-click">
+            <button class="w3-button w3-block w3-left-align" onclick="accordion('manage_books')">
+                <i class="fa fa-cog" aria-hidden="true"></i>
+                Gérer les livres
+                <i class="fa fa-caret-down" aria-hidden="true"></i>
+            </button>
 
-                <button class="w3-button" onclick="open_or_close_dropdown('manage_books')">
-                    <i class="fa fa-cog" aria-hidden="true"></i>
-                    Gérer les livres
-                    <i class="fa fa-caret-down" aria-hidden="true"></i>
-                </button>
+            <div class="w3-hide w3-white" id="manage_books">
 
-                <div class="w3-dropdown-content w3-bar-block" id="manage_books">
+                <a class="w3-bar-item w3-button" href="<?= $this->create_url('upload_book'); ?>">
+                    <i class="fa fa-plus" aria-hidden="true"></i>
+                    Ajouter un livre
+                </a>
 
-                    <a class="w3-bar-item w3-button" href="<?= $this->create_url('upload_book'); ?>">
-                        <i class="fa fa-plus" aria-hidden="true"></i>
-                        Ajouter un livre
-                    </a>
+                <a class="w3-bar-item w3-button" href="<?= $this->create_url('delete_book'); ?>">
+                    <i class="fa fa-trash" aria-hidden="true"></i>
+                    Supprimer un livre
+                </a>
 
-                    <a class="w3-bar-item w3-button" href="<?= $this->create_url('delete_book'); ?>">
-                        <i class="fa fa-trash" aria-hidden="true"></i>
-                        Supprimer un livre
-                    </a>
-
-                    <a class="w3-bar-item w3-button" href="<?= $this->create_url('undelete_book'); ?>">
-                        <i class="fa fa-undo" aria-hidden="true"></i>
-                        Annuler la suppression
-                    </a>
-
-                </div>
+                <a class="w3-bar-item w3-button" href="<?= $this->create_url('undelete_book'); ?>">
+                    <i class="fa fa-undo" aria-hidden="true"></i>
+                    Annuler la suppression
+                </a>
 
             </div>
 
-            <div class="w3-dropdown-click">
+            <button class="w3-button w3-block w3-left-align" onclick="accordion('my_account')">
+                <i class="fa fa-user" aria-hidden="true"></i>
+                Gérer mon compte
+                <i class="fa fa-caret-down" aria-hidden="true"></i>
+            </button>
 
-                <button class="w3-button" onclick="open_or_close_dropdown('my_account')">
-                    <i class="fa fa-user" aria-hidden="true"></i>
-                    Gérer mon compte
-                    <i class="fa fa-caret-down" aria-hidden="true"></i>
-                </button>
+            <div class="w3-hide w3-white" id="my_account">
 
-                <div class="w3-dropdown-content w3-bar-block" id="my_account">
+                <a class="w3-bar-item w3-button" href="<?= $this->create_url('change_email'); ?>">
+                    <i class="fa fa-at" aria-hidden="true"></i>
+                    Changer d'adresse e-mail
+                </a>
 
-                    <a class="w3-bar-item w3-button" href="<?= $this->create_url('change_email'); ?>">
-                        <i class="fa fa-at" aria-hidden="true"></i>
-                        Changer d'adresse e-mail
-                    </a>
-
-                    <a class="w3-bar-item w3-button" href="<?= $this->create_url('change_password'); ?>">
-                        <i class="fa fa-key" aria-hidden="true"></i>
-                        Changer de mot de passe
-                    </a>
-
-                </div>
+                <a class="w3-bar-item w3-button" href="<?= $this->create_url('change_password'); ?>">
+                    <i class="fa fa-key" aria-hidden="true"></i>
+                    Changer de mot de passe
+                </a>
 
             </div>
 
